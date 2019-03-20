268059e36da34c4f9bf2c18b767f8d0e5f11540b
Luis Moral Guerrero <luis.moral@gmail.com>
2019-03-20 05:28:54 +0100

    Refactoring.

--- src/main/java/es/molabs/bocli/parser/ConsoleCommandParser.java
+++ src/main/java/es/molabs/bocli/parser/ConsoleCommandParser.java
@@ -3,10 +3,10 @@ package es.molabs.bocli.parser;
 import es.molabs.bocli.client.WebClient;
 import es.molabs.bocli.command.Command;
 import es.molabs.bocli.command.ErrorParsingCommand;
-import es.molabs.bocli.command.ListCreatorsCommand;
 import es.molabs.bocli.command.ShowHelpCommand;
 import es.molabs.bocli.ouput.Output;
 import es.molabs.bocli.parser.definition.CreateNoteCommandDefinition;
+import es.molabs.bocli.parser.definition.ListCreatorsCommandDefinition;
 import org.apache.commons.cli.*;
 
 import java.io.PrintWriter;
@@ -20,9 +20,6 @@ public class ConsoleCommandParser implements CommandParser<String[]> {
     private static final String COMMAND_HELP = "help";
     private static final String COMMAND_LIST = "list";
     private static final String COMMAND_CREATE_NOTE = "add";
-
-    private static final String COMMAND_LIST_FILTER = "filter";
-    private static final String COMMAND_LIST_SORT = "sort";
 
     private final WebClient webClient;
     private final Output output;
@@ -47,14 +44,6 @@ public class ConsoleCommandParser implements CommandParser<String[]> {
         return actions;
     }
 
-    private Options listCommandOptions() {
-        Options options = new Options();
-        options.addOption(Option.builder(COMMAND_LIST_FILTER).desc("Field to filter").numberOfArgs(2).valueSeparator('=').build());
-        options.addOption(COMMAND_LIST_SORT, true, "Field to sort");
-
-        return options;
-    }
-
     private Option actionOption(String argument, String description) {
         return
             Option
@@ -77,12 +66,8 @@ public class ConsoleCommandParser implements CommandParser<String[]> {
             }
             else if (actionLine.hasOption(COMMAND_LIST)) {
                 command =
-                    parseListCommand(
-                        Arrays
-                            .stream(args)
-                            .filter(value -> !value.equals("-" + COMMAND_LIST))
-                            .toArray(length -> new String[length])
-                    );
+                    new ListCreatorsCommandDefinition()
+                        .parse(output, webClient, DEFAULT_HOST, parser, removeArgument(args, COMMAND_LIST));
             }
             else if (actionLine.hasOption(COMMAND_CREATE_NOTE)) {
                 command =
@@ -105,20 +90,6 @@ public class ConsoleCommandParser implements CommandParser<String[]> {
                 .stream(args)
                 .filter(value -> !value.equals("-" + argument))
                 .toArray(length -> new String[length]);
-    }
-
-    private Command parseListCommand(String[] args) throws ParseException {
-        CommandLine line = parser.parse(listCommandOptions(), args);
-        String[] filter = line.getOptionValues(COMMAND_LIST_FILTER);
-
-        return
-            new ListCreatorsCommand(
-                output,
-                webClient,
-                DEFAULT_HOST,
-                filter != null ? filter[0] : null,
-                filter != null ? filter[1] : null,
-                line.getOptionValue(COMMAND_LIST_SORT));
     }
 
     private String buildHelpMessage() {
---
+++ src/main/java/es/molabs/bocli/parser/definition/ListCreatorsCommandDefinition.java
@@ -0,0 +1,37 @@
+package es.molabs.bocli.parser.definition;
+
+import es.molabs.bocli.client.WebClient;
+import es.molabs.bocli.command.ListCreatorsCommand;
+import es.molabs.bocli.ouput.Output;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.ParseException;
+
+public class ListCreatorsCommandDefinition extends ConsoleCommandDefinition<ListCreatorsCommand> {
+
+    private static final String ARGUMENT_FILTER = "filter";
+    private static final String ARGUMENT_SORT = "sort";
+
+    public ListCreatorsCommandDefinition() {
+        super(
+            Option.builder(ARGUMENT_FILTER).desc("Field to filter").numberOfArgs(2).valueSeparator('=').build(),
+            Option.builder(ARGUMENT_SORT).hasArg().desc("Field to sort").build()
+        );
+    }
+
+    @Override
+    public ListCreatorsCommand parse(Output output, WebClient webClient, String host, CommandLineParser parser, String[] args) throws ParseException {
+        CommandLine line = parser.parse(options(), args);
+        String[] filter = line.getOptionValues(ARGUMENT_FILTER);
+
+        return
+            new ListCreatorsCommand(
+                output,
+                webClient,
+                host,
+                filter != null ? filter[0] : null,
+                filter != null ? filter[1] : null,
+                line.getOptionValue(ARGUMENT_SORT));
+    }
+}
